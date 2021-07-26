2c7574baca15a0ad3d0f50660388ceaf183be88f
badbayan <badya65@gmail.com>
2021-07-26 20:29:13 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/badbayan/statusloop
+
+go 1.16
+
+require github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240
---
+++ statusloop.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/jezek/xgb"
+	"github.com/jezek/xgb/xproto"
+)
+
+func main() {
+	wmname := ""
+	xconn, err := xgb.NewConn()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer xconn.Close()
+	xroot := xproto.Setup(xconn).DefaultScreen(xconn).Root
+	defer xproto.ChangeProperty(xconn, xproto.PropModeReplace, xroot, xproto.AtomWmName,
+		xproto.AtomString, 8, uint32(len(wmname)), []byte(wmname))
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	go func() {
+		for scanner.Scan() {
+			if len(scanner.Text()) == 0 {
+				break
+			}
+			wmname = " " + scanner.Text() + " "
+			xproto.ChangeProperty(xconn, xproto.PropModeReplace, xroot, xproto.AtomWmName,
+				xproto.AtomString, 8, uint32(len(wmname)), []byte(wmname))
+		}
+		sigs <- os.Interrupt
+	}()
+	<-sigs
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			log.Fatal(err)
+		}
+	}
+}
